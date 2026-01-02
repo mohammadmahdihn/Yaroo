4d2fffba7b3e661092fb4ae4f5ab563f9c7e997a
mohammadmahdihn <mohammadmahdiheydarinasab@gmail.com>
2026-01-02 19:17:47 +0330

    Add mmWave transmission rate calculation

--- proj.py
+++ proj.py
@@ -1,5 +1,3 @@
-import numpy as np
-from scipy.optimize import minimize
 import math
 import random
 from scipy.stats import truncnorm
@@ -12,15 +10,30 @@ sigma = 10  # standard deviation of speed
 mm_wave_comm_range = 150
 cellular_link_comm_range = 200
 max_position = r * n
+rsu_max_data_rate = 1_000_000_000
 
 # cellular transmission rate variables
 W_uu = 20_000_000  # cellular channel bandwidth
-noise_density_dbm_per_hz = -174.0
-noise_figure_db = 7.0
+cellular_noise_density_dbm_per_hz = -174.0
+cellular_noise_figure_db = 7.0
 h_abs_sq = 1.0  # |h^2|
 cellular_path_loss_exponent = 3.2
 
+# mmWave transmission rate variables
 W_mm = 200_000_000  # mmWave channel bandwidth
+mmWave_noise_density_dbm_per_hz = -174.0
+mmWave_noise_figure_db = 7.0
+mmWave_path_loss_exponent = 3.2
+shadow_fading_db = 3.0  # ρα
+Gmax_vehicle_db = 15.0  # RSU main-lobe gain (dB)
+Gmax_rsu_db = 15.0  # Vehicle main-lobe gain (dB)
+
+def w_to_dbm(p_w):
+    return 10.0 * math.log10(p_w * 1000.0)
+
+def db_to_linear(db: float) -> float:
+    return 10.0 ** (db / 10.0)
+
 
 class Vehicle:
     def __init__(self, speed_distribution):
@@ -33,20 +46,29 @@ class Vehicle:
     def calculate_transmission_rate(self):
         distance = r * math.ceil(self.s / r) - self.s
         cellular = self.calculate_cellular_transmission_rate(distance)
-        mmWave = self.calculate_mm_wave_transmission_rate(distance)
-        return
+        mmWave = self.calculate_mmWave_transmission_rate(distance)
+        return min(max(cellular, mmWave), rsu_max_data_rate)
 
     def calculate_cellular_transmission_rate(self, distance):
         d = max(distance, 1e-9)
 
-        noise_dbm = noise_density_dbm_per_hz + noise_figure_db + 10.0 * math.log10(W_uu)
+        noise_dbm = cellular_noise_density_dbm_per_hz + cellular_noise_figure_db + 10.0 * math.log10(W_uu)
         noise_w = 10.0 ** ((noise_dbm - 30.0) / 10.0)  # dBm -> W
 
         snr = (self.p_i * (d ** (-cellular_path_loss_exponent)) * h_abs_sq) / noise_w
         return W_uu * math.log2(1.0 + snr)
 
     def calculate_mmWave_transmission_rate(self, distance):
-        pass
+        d = max(distance, 1e-9)
+
+        p_dbm = w_to_dbm(self.p_i)
+        noise_power_dbm = (mmWave_noise_density_dbm_per_hz + mmWave_noise_figure_db) + 10.0 * math.log10(W_mm)
+
+        path_loss_db = 10.0 * mmWave_path_loss_exponent * math.log10(d) + 69.6 + shadow_fading_db
+        snr_db = (p_dbm - noise_power_dbm) + (Gmax_vehicle_db + Gmax_rsu_db) - path_loss_db
+
+        snr = db_to_linear(snr_db)
+        return W_mm * math.log2(1.0 + snr)
 
 
 v_min, v_max = mu - 3 * sigma, mu + 3 * sigma
@@ -54,3 +76,4 @@ a, b = (v_min - mu) / sigma, (v_max - mu) / sigma
 dist = truncnorm(a, b, loc=mu, scale=sigma)
 
 vehicles = [Vehicle(dist) for _ in range(n)]
+print(vehicles)
